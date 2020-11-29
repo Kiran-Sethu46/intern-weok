79f3c784657f46749bf56895b5f0c7128f99f179
kiran-Sethu46 <kiran226242@gmail.com>
2020-11-29 23:05:09 +0530

    added mt firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVB9Z2FnSRlcSWkOq1cLDd84BAgRzWhXg",
-  authDomain: "apna-clone.firebaseapp.com",
-  databaseURL: "https://apna-clone.firebaseio.com",
-  projectId: "apna-clone",
-  storageBucket: "apna-clone.appspot.com",
-  messagingSenderId: "1033418306067",
-  appId: "1:1033418306067:web:b34c05e2df9f51543845e6",
-  measurementId: "G-MDLZCRR86F",
+  apiKey: "AIzaSyCLMkGEwu80To7jFjx-9KiAc02UcbMCtaw",
+  authDomain: "clone-9f1e2.firebaseapp.com",
+  databaseURL: "https://clone-9f1e2.firebaseio.com",
+  projectId: "clone-9f1e2",
+  storageBucket: "clone-9f1e2.appspot.com",
+  messagingSenderId: "186019838613",
+  appId: "1:186019838613:web:858595c7b3842b6a1a799c",
+  measurementId: "G-4MY84ZS336"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
